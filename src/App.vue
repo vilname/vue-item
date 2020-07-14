9c003d822dcae09ab37c37fb2bde5c23b97e88c5
Alexander <test@test.ru>
2020-07-14 22:56:36 +0500

    make it better

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    appHeader: Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import {routes} from './routes.js'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+Vue.use(VueAxios, axios)
+Vue.use(IconsPlugin)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
